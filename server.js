f7d59f6aecb92f5c44d154d300bfbf2a1638f3b4
cmksj1005 <cmksj1005@gmail.com>
2023-08-11 18:51:31 -0400

    update assignment 6
    update comments

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 /*********************************************************************************
- *  WEB322 – Assignment 05
+ *  WEB322 – Assignment 06
  *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source
  *  (including 3rd party web sites) or distributed to other students.
  *
- *  Name: Shinjo Kang Student ID: 118135227 Date: 2023-07-25
+ *  Name: Shinjo Kang Student ID: 118135227 Date: 2023-08-11
  *
  *  Cyclic Web App URL: https://tasty-puce-cougar.cyclic.app
  *
